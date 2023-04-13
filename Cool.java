87ec72576797e574a0013599a29b769750fd24f0
CoolingY <1848062200@qq.com>
2023-04-13 18:34:13 +0800

    Cool change to Git Cool

--- Cool.java
+++ Cool.java
@@ -1,7 +1,7 @@
 public class Cool {
     public static void main(String[] args) {
         System.out.println("Cool Git!");
-        System.out.println("Cool!");
+        System.out.println("Git Cool!");
         System.out.println("Cool GitHub!");
     }
 }
